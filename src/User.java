f628e27710bb7e01be318079745ee1e4f0ca650b
Zach Morse <zach.morse@qut.edu.au>
2024-09-05 16:23:06 +1000

    Added ToDoItem class

--- src/User.java
+++ src/User.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+
 public class User {
     private String username;
     private String password;
 
+    private ArrayList<ToDoItem> toDoItems;
+
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.toDoItems = new ArrayList<>();
     }
 
     /**
